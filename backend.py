c5f97287d0d3fa23b5e06f03d079f32de40eaf8f
Hamza Alawiye <hamza.alawiye@maths.ox.ac.uk>
2017-05-21 22:20:56 +0000

    Version 1.20 initial solo/team rating separation (and minor bugfix)

--- backend.py
+++ backend.py
@@ -12,18 +12,25 @@ def newPlayer(nom):
         win_pct = None,
         streak = 0,
         rating = 1500,
+        team_rating = 1500,
         rd = 350,
+        team_rd = 350,
         tau = 0.06,
+        team_tau = 0.06,
         streak_peak = 0,
         streak_trough = 0,
         rating_peak = 1500,
+        team_rating_peak = 1500,
         rating_trough = 1500,
+        team_rating_trough = 1500,
         differential_peak = 0,
         differential_trough = 0,
         sp_obtained = epoch,
         st_obtained = epoch,
         rp_obtained = epoch,
+        trp_obtained = epoch,
         rt_obtained = epoch,
+        trt_obtained = epoch,
         dp_obtained = epoch,
         dt_obtained = epoch)
     db.session.add(player)
@@ -41,18 +48,25 @@ def recalculate():
         p.win_pct = None
         p.streak = 0
         p.rating = 1500
+        p.team_rating = 1500
         p.rd = 350
+        p.team_rd = 350
         p.tau = 0.06
+        p.team_tau = 0.06
         p.streak_peak = 0
         p.streak_trough = 0
         p.rating_peak = 1500
+        p.team_rating_peak = 1500
         p.rating_trough = 1500
+        p.team_rating_trough = 1500
         p.differential_peak = 0
         p.differential_trough = 0
         p.sp_obtained = epoch
         p.st_obtained = epoch
         p.rp_obtained = epoch
+        p.trp_obtained = epoch
         p.rt_obtained = epoch
+        p.trt_obtained = epoch
         p.dp_obtained = epoch
         p.dt_obtained = epoch
         p.last_played = None
@@ -70,14 +84,16 @@ def recalculate():
             for pid in teams[i]:
                 p = Player.query.get(pid)
                 if p is None:
-                    break
+                    continue
                 ps.append(p)
                 if ((i == 0 and game.oneWin) or (i == 1 and not game.oneWin)):
                     wins.append(1)
                 else:
                     wins.append(0)
 
-        newRatingPeriod(players)
+        game.n_players = len(ps)
+
+        newRatingPeriod(players, len(ps))
         changes = updateGlicko(ps, wins, game.date)
         for i in range(len(ps)):
             updateIndividual(ps[i],wins[i], game.date)
--- flask_app.py
+++ flask_app.py
@@ -36,6 +36,7 @@ class Game(db.Model):
     diff1b = db.Column(db.Float)
     diff2a = db.Column(db.Float)
     diff2b = db.Column(db.Float)
+    n_players = db.Column(db.Integer)
 
 class Player(db.Model):
 
@@ -50,19 +51,26 @@ class Player(db.Model):
     win_pct = db.Column(db.Float)
     streak = db.Column(db.Integer)
     rating = db.Column(db.Float)
+    team_rating = db.Column(db.Float)
     rd = db.Column(db.Float)
+    team_rd = db.Column(db.Float)
     tau = db.Column(db.Float)
+    team_tau = db.Column(db.Float)
     streak_peak = db.Column(db.Integer)
     streak_trough = db.Column(db.Integer)
     rating_peak= db.Column(db.Float)
+    team_rating_peak = db.Column(db.Float)
     rating_trough = db.Column(db.Float)
+    team_rating_trough = db.Column(db.Float)
     differential_peak = db.Column(db.Integer)
     differential_trough = db.Column(db.Integer)
     last_played = db.Column(db.DateTime)
     sp_obtained = db.Column(db.DateTime)
     st_obtained = db.Column(db.DateTime)
     rp_obtained = db.Column(db.DateTime)
+    trp_obtained = db.Column(db.DateTime)
     rt_obtained = db.Column(db.DateTime)
+    trt_obtained = db.Column(db.DateTime)
     dp_obtained = db.Column(db.DateTime)
     dt_obtained = db.Column(db.DateTime)
 
@@ -91,17 +99,35 @@ def updateIndividual(p,win, game_time):
             p.st_obtained = game_time
     p.win_pct = 100*p.wins/p.played
 
-def newRatingPeriod(players):
+def newRatingPeriod(players, n_players):
     n_inactive = 2000
     csq = (350**2 - 50**2)/n_inactive
-    for p in players:
-        p.rd = min(math.sqrt((p.rd)**2 + csq),350)
-
+    if n_players < 4:
+        for p in players:
+            p.rd = min(math.sqrt((p.rd)**2 + csq),350)
+    if n_players > 2:
+        for p in players:
+            p.team_rd = min(math.sqrt((p.team_rd)**2 + csq),350)
 
 def updateGlicko(ps, wins, game_time):
     #validate lists
-    winners = [(ps[i],ps[i].rating,ps[i].rd) for i in range(len(ps)) if wins[i] == 1]
-    losers = [(ps[i],ps[i].rating,ps[i].rd) for i in range(len(ps)) if wins[i] == 0]
+    ws = [ps[i] for i in range(len(ps)) if wins[i] == 1]
+    ls = [ps[i] for i in range(len(ps)) if wins[i] == 0]
+
+    w_solo = (len(ws) == 1)
+    l_solo = (len(ls) == 1)
+    winners = []
+    losers = []
+
+
+    if w_solo:
+        winners = [(w, w.rating, w.rd) for w in ws]
+    else:
+        winners = [(w, w.team_rating, w.team_rd) for w in ws]
+    if l_solo:
+        losers = [(l, l.rating, l.rd) for l in ls]
+    else:
+        losers = [(l, l.team_rating, l.team_rd) for l in ls]
 
     w_ensemble = 0;
     w_rd = 0;
@@ -132,13 +158,21 @@ def updateGlicko(ps, wins, game_time):
         recip = 1.0/(1.0/(rd*rd) + 1.0/dsq)
         diff = q*recip*g(l_rd)*(1-e)
 
-        p.rating = r + diff
-        p.rd = math.sqrt(recip)
+        if w_solo:
+            p.rating = r + diff
+            p.rd = math.sqrt(recip)
+            if(p.rating > p.rating_peak and p.rd <= 110):
+                p.rating_peak = p.rating
+                p.rp_obtained = game_time
+        else:
+            p.team_rating = r + diff
+            p.team_rd = math.sqrt(recip)
+            if(p.team_rating > p.team_rating_peak and p.team_rd <= 110):
+                p.team_rating_peak = p.team_rating
+                p.trp_obtained = game_time
 
         changes.append((p,diff))
-        if(p.rating > p.rating_peak and p.rd <= 110):
-            p.rating_peak = p.rating
-            p.rp_obtained = game_time
+
 
     for p,r,rd in losers:
         e = e_s(l_ensemble, w_ensemble, w_rd)
@@ -147,13 +181,20 @@ def updateGlicko(ps, wins, game_time):
         recip = 1.0/(1.0/(rd*rd) + 1.0/dsq)
         diff = q*recip*g(w_rd)*(-e)
 
-        p.rating = r + diff
-        p.rd = math.sqrt(recip)
+        if l_solo:
+            p.rating = r + diff
+            p.rd = math.sqrt(recip)
+            if(p.rating < p.rating_trough and p.rd <= 110):
+                p.rating_trough = p.rating
+                p.rt_obtained = game_time
+        else:
+            p.team_rating = r + diff
+            p.team_rd = math.sqrt(recip)
+            if(p.team_rating < p.team_rating_trough and p.team_rd <= 110):
+                p.team_rating_trough = p.team_rating
+                p.trt_obtained = game_time
 
         changes.append((p,diff))
-        if(p.rating < p.rating_trough and p.rd <= 110):
-            p.rating_trough = p.rating
-            p.rt_obtained = game_time
 
     return changes
 
@@ -225,7 +266,8 @@ def index():
         diff1a = 0,
         diff1b = 0,
         diff2a = 0,
-        diff2b = 0)
+        diff2b = 0,
+        n_players = 0)
 
     #validation goes here
     ids = [game.player1a, game.player1b, game.player2a, game.player2b]
@@ -264,9 +306,11 @@ def index():
                 wins.append(1)
             else:
                 wins.append(0)
+    game.n_players = len(ps)
+
 
     now = datetime.utcnow()
-    newRatingPeriod(players)
+    newRatingPeriod(players, len(ps))
     changes = updateGlicko(ps, wins, now)
     for i in range(len(ps)):
         updateIndividual(ps[i],wins[i], now)
@@ -304,22 +348,22 @@ def stats():
 
 @app.route('/leaderboard', methods=['GET'])
 def leaderboard():
-    leader_stats = [("rating","Rating",True), ("win_pct","Win Percentage",True), ("streak","Streak",True)]
+    leader_stats = [("rating","Rating",True), ("team_rating","Team Rating",True), ("win_pct","Win Percentage",True), ("streak","Win/Loss Streak",True)]
     if request.method == "GET":
         players=Player.query.all()
-        leaders = {k : (n,sorted([(p.pid, p.name, vars(p)[k], p.rd) for p in players],key = lambda tup: -1 if tup[2] is None else tup[2], reverse = b)) for k,n,b in leader_stats}
+        leaders = {k : (n,sorted([(p.pid, p.name, vars(p)[k], p.rd, p.team_rd) for p in players],key = lambda tup: -1 if tup[2] is None else tup[2], reverse = b)) for k,n,b in leader_stats}
         leaders_list = list(leaders.items())
-        leaders_list.sort()
-        return render_template("leaderboard.html", leaders=leaders.items())
+        leaders_list.sort(key = lambda tup: tup[1][0])
+        return render_template("leaderboard.html", leaders=leaders_list)
 
 @app.route('/hof', methods=['GET'])
 def hof():
-    record_stats = [("played","Most Games Played",max), ("rating_peak","Highest Rating Achieved",max), ("rating_trough","Lowest Rating Achieved",min), ("streak_peak","Longest Win Streak",max), ("streak_trough","Longest Loss Streak",min)]
+    record_stats = [("played","Most Games Played",max), ("rating_peak","Highest Rating Achieved",max), ("rating_trough","Lowest Rating Achieved",min), ("team_rating_peak","Highest Team Rating Achieved",max), ("team_rating_trough","Lowest Team Rating Achieved",min), ("streak_peak","Longest Win Streak",max), ("streak_trough","Longest Loss Streak",min)]
     if request.method == "GET":
         players=Player.query.all()
         vs = {n: f([(vars(p)[k],p.name) for p in players],key = lambda tup : tup[0]) for k,n,f in record_stats}
         vs_list = list(vs.items())
-        vs_list.sort()
+        vs_list.sort(key = lambda tup : [t[1] for t in record_stats].index(tup[0]))
         return render_template("hof.html", vs=vs_list)
 
 @app.route('/games', methods=['GET'])
@@ -337,20 +381,38 @@ def history():
     gs = Game.query.order_by(Game.date).all()
     players = Player.query.all()
     name_dict = {p.pid : p.name for p in players}
+    #should validate instead
     name_dict[0] = ""
     hist_dict = {p.name:([0],[1500]) for p in players}
+    team_hist_dict = {p.name:([0],[1500]) for p in players}
     #should probably validate the below
     for n,g in enumerate(gs):
-        for tup in [(g.player1a,g.diff1a),(g.player1b,g.diff1b),(g.player2a,g.diff2a),(g.player2b,g.diff2b)]:
-            p = name_dict[tup[0]]
-            if p == "":
-                continue
-            hist_dict[p][0].append(n+1)
-            hist_dict[p][1].append(hist_dict[p][1][-1]+tup[1])
+        teams = [[(g.player1a,g.diff1a),(g.player1b,g.diff1b)],[(g.player2a,g.diff2a),(g.player2b,g.diff2b)]]
+
+        for i in range(2):
+            solo = False
+            #below won't work if we expand to larger teams
+            if 0 in [tup[0] for tup in teams[i]]:
+                solo = True
+            for tup in teams[i]:
+                pid = tup[0]
+                if pid == 0:
+                    continue
+                p = name_dict[pid]
+                if solo:
+                    hist_dict[p][0].append(n+1)
+                    hist_dict[p][1].append(hist_dict[p][1][-1]+tup[1])
+                else:
+                    team_hist_dict[p][0].append(n+1)
+                    team_hist_dict[p][1].append(team_hist_dict[p][1][-1]+tup[1])
 
     hist_list = list(hist_dict.items())
     hist_list.sort(key = lambda t : t[0])
     n_games = [len(tup[0]) for _,tup in hist_list]
+
+    team_hist_list = list(team_hist_dict.items())
+    team_hist_list.sort(key = lambda t : t[0])
+    team_n_games = [len(tup[0]) for _,tup in team_hist_list]
 
     def playerColour(i,n):
         seed = 0.8938123179670188
@@ -371,7 +433,8 @@ def history():
         }[h_i]
         return (int(r*256), int(g*256), int(b*256),1)
 
+    histories = [("rating", "Rating", hist_list, len(hist_dict), n_games),("team_rating","Team Rating", team_hist_list, len(team_hist_dict), team_n_games)]
 
 
-    return render_template("history.html",hist=hist_list,n = len(hist_dict), m = n_games, playerColour=playerColour)
+    return render_template("history.html",histories=histories, playerColour=playerColour)
 
